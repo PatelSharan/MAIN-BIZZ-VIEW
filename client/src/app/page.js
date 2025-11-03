473d1d2e70626a32cb6e0c2515e49335dad79c27
sharan patel <sharanpatel4@gmail.com>
2025-11-03 15:14:42 +0530

    MAIN - ADDED animate number

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,20 @@
+"use client"
 import AOSWrapper from "@/components/common/AOSWrapper";
 import Heading from "@/components/layouts/Heading";
 import ProductBox from "@/components/layouts/ProductBox";
 import Image from "next/image";
 import Link from "next/link";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+
 
 export default function Home() {
+
+  const { ref: highlightsRef, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+
 
   return (
     <>
@@ -160,6 +170,44 @@ export default function Home() {
           </div>
         </section>
       </AOSWrapper>
+
+
+      {/* <<=== MAIN = Highlights */}
+      <section className="py-20 bg-primary-bgColor/90" ref={highlightsRef}>
+        <div className="space-y-10 layout-container text-theme-text">
+          <Heading
+            desc={"Highlights"}
+            className={"text-center"}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+            {[
+              { title: "17", suffix: "+", label: "Years Of Business" },
+              { title: "100", suffix: "+", label: "Products In Range", duration: 6 },
+              { title: "70", suffix: "+", label: "Clients", duration: 4 },
+              { title: "10", label: "Awards Won" },
+            ].map((item, idx) => (
+              <div
+                key={idx}
+                className="p-10 space-y-3 border"
+              >
+                <div className="font-semibold text-3xl text-left">
+                  {inView ? (
+                    <CountUp
+                      start={0}
+                      end={item?.title}
+                      duration={item?.duration || 2}
+                      suffix={item?.suffix}
+                    />
+                  ) : (
+                    0
+                  )}
+                </div>
+                <div>{item.label}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
