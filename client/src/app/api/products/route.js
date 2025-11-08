d850258aa73da1e4a080a84c82a28842de7e0f37
sharan patel <sharanpatel4@gmail.com>
2025-11-08 23:57:43 +0530

    MAIN - setup env variables for access google sheet in vercel..

--- client/src/app/api/products/route.js
+++ client/src/app/api/products/route.js
@@ -5,7 +5,6 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try {
         const auth = new google.auth.GoogleAuth({
-            keyFile: "bizzview-service-acc-key.json",
             credentials: {
                 client_email: process.env.GOOGLE_CLIENT_EMAIL,
                 private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
